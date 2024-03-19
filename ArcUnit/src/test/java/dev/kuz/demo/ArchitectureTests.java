eb90920df325a937eb10abf3301fb45cec5e797a
Yaroslav Kuzenko <yaroslavkuzenko02@gmail.com>
2024-03-19 20:06:02 +0200

    new branch

---
+++ ArcUnit/src/test/java/dev/kuz/demo/ArchitectureTests.java
@@ -0,0 +1,11 @@
+package dev.kuz.demo;
+/*
+  @author   yaroslavkuzenko
+  @project   ArcUnit
+  @class  ArchitectureTests
+  @version  1.0.0 
+  @since 19.03.2024 - 20.04
+*/
+
+public class ArchitectureTests {
+}
